84302000799ad85cc983d386d3dae6e82f7bb1f3
OlgaSavitsk <olbonya.89@mail.ru>
2023-01-04 13:58:41 +0300

    feat: change base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://localhost:4000',
-  // GOOGLE_CLIENT_ID: '58278291806-gk1kr5l925plgihas1917prajuoed9da.apps.googleusercontent.com'
+  BASE_URL: 'https://review-project.onrender.com',
 };
 
 /*
