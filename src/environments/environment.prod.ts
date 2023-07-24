2a0ed15faa7523a3286072d657abcace50a1c5a4
olga <olbonya.89@mail.ru>
2023-07-24 21:49:43 +0300

    feat: change server deploy url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://app-reviews-server-production.up.railway.app',
-  GIT_HUB_URL: 'https://app-reviews-server-production.up.railway.app/auth/github',
-  GOOGLE_URL: 'https://app-reviews-server-production.up.railway.app/auth/google',
+  BASE_URL: 'https://review-project.onrender.com',
+  GIT_HUB_URL: 'https://review-project.onrender.com/auth/github',
+  GOOGLE_URL: 'https://review-project.onrender.com/auth/google',
 };
