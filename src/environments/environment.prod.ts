6be1468ebdd77683b11e591f26bb3a7c8bc4a72f
Olga <78305019+OlgaSavitsk@users.noreply.github.com>
2023-12-01 01:11:23 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'app-reviews-server-production.up.railway.app',
+  BASE_URL: 'https://review-project.onrender.com',
   GIT_HUB_URL: 'app-reviews-server-production.up.railway.app/auth/github',
   GOOGLE_URL: 'app-reviews-server-production.up.railway.app/auth/google',
 };
